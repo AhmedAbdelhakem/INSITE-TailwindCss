b9b5b51e56b8c6a3d046eea2d3ffeaf5ede030b1
AhmedAbdelhakem <100963499+AhmedAbdelhakem@users.noreply.github.com>
2023-11-06 18:29:23 +0200

    second Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,25 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
-      width: {
-        '1300': '1300px', 
-      },
-      height: {
-        '400': '400px',
-      },
       colors: {
         primary: '#fcaf2a',
         secondary: '#545F71',
         borderColor: '#343a40',
+        PicksColor: '#B4B4B4',
+        textWarning: '#ffc107',
+        textMuted: '#6c757d',
       },
       fontFamily: {
-        display: ['Red Hat Display'],
+        sans: ['Red Hat Display'],
       },
     },
+    container: {
+      center: true,
+      padding: '20px',
+    },
+    maxWidth: {
+      'xl': '1260px',
+    }
   },
   plugins: [],
 }
